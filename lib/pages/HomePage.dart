1461b42cb719b3761896a2a87f333369efd1e881
Your Name <you@example.com>
2024-12-14 05:23:14 -0600

    Second Commit by Ward Chnges to HomePage

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,10 +1,11 @@
-// ignore: file_names
+// ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
 import 'package:pl_project/models/btn.dart' as btn;
 import 'package:pl_project/pages/OrdersPage.dart';
 import 'package:pl_project/pages/ProfilePage.dart';
 import 'package:pl_project/pages/SearchPage.dart';
+import 'package:pl_project/pages/ShopsPage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -103,97 +104,145 @@ class HomePageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
+    return Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 25.0),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    height: 40.0,
-                    color: Colors.amber[300],
-                    child: const Center(
-                      child: Text(
-                        'Categories',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 20.0,
-                        ),
+        child: Column(children: [
+          const SizedBox(height: 25.0),       //Categories COntainer
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: 40.0,
+                  color: Colors.amber[300],
+                  child: const Center(
+                    child: Text(
+                      'Categories',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 20.0,
                       ),
                     ),
                   ),
-                  Container(
-                    height: 70.0,
-                    color: Colors.amber[900],
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Expanded(
-                            child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.home,
-                            size: 30,
-                          ),
-                        )),
-                        Expanded(
-                            child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.home,
-                            size: 30,
-                          ),
-                        )),
-                        Expanded(
-                            child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.home,
-                            size: 30,
-                          ),
-                        )),
-                        Expanded(
-                            child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.home,
-                            size: 30,
-                          ),
-                        )),
-                        Expanded(
-                            child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.home,
-                            size: 30,
-                          ),
-                        )),
-                      ],
-                    ),
+                ),
+                Container(
+                  height: 70.0,
+                  color: Colors.amber[900],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                          child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.home,
+                          size: 30,
+                        ),
+                      )),
+                      Expanded(
+                          child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.home,
+                          size: 30,
+                        ),
+                      )),
+                      Expanded(
+                          child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.home,
+                          size: 30,
+                        ),
+                      )),
+                      Expanded(
+                          child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.home,
+                          size: 30,
+                        ),
+                      )),
+                      Expanded(
+                          child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.home,
+                          size: 30,
+                        ),
+                      )),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const SizedBox(height: 50.0),
+          ),
+          const SizedBox(height: 50.0),      //Stores Header
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children:[
+              CircleAvatar(child: Icon(Icons.storefront_sharp)),
+              SizedBox(width: 8.0,),
+              Text(
+                'Stores around you...',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  //color: Colors.blueGrey
+                ),
+                ),
+            ],
+          ),
+          const SizedBox(),                 //Stores Grid View
+          Flexible(
+              child: GridView.count(
+            padding: const EdgeInsets.all(4),
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            crossAxisCount: 2,
+            children: [
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: const ButtonImage()),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: const ButtonImage()),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: const ButtonImage()),
+              Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: const ButtonImage()),
+            ],
+          ))
+        ]));
+  }
+}
+
+class ButtonImage extends StatelessWidget {
+  const ButtonImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        child: Stack(
+          alignment: const Alignment(0.0, 0.8),
+          children: [
+            //  Image.asset('iuiapiuia')                 //the shop photo is here
             Container(
-              decoration: BoxDecoration(
-                border: Border.all(),
-              ),
-              child: const Column(
-                children: [],
-              ),
+              decoration: BoxDecoration(border: Border.all()),
+              child: const Text('the name of the shop'),
             ),
           ],
         ),
-      ),
-    );
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ShopsPage()));
+        });
   }
 }
